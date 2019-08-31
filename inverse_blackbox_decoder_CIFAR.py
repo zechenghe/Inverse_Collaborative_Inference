19ba0f3d4dd1a25cbacc7216a41471aaf62b56cf
zechengh <zcheustc@gmail.com>
2019-08-31 23:11:36 +0800

    ReLU22

--- inverse_blackbox_decoder_CIFAR.py
+++ inverse_blackbox_decoder_CIFAR.py
@@ -23,10 +23,10 @@ from utils import *
 #####################
 # Note:
 # Training:
-# python inverse_blackbox_decoder_CIFAR.py --layer conv22 --iter 50 --training --decodername CIFAR10CNNDecoderconv22
+# python inverse_blackbox_decoder_CIFAR.py --layer ReLU22 --iter 50 --training --decodername CIFAR10CNNDecoderReLU22
 #
 # Testing:
-# python inverse_blackbox_decoder_CIFAR.py --testing --decodername CIFAR10CNNDecoderconv22 --layer conv22
+# python inverse_blackbox_decoder_CIFAR.py --testing --decodername CIFAR10CNNDecoderReLU22 --layer ReLU22
 #####################
 
 
