cd2d2fd61085c90482fc7b6fadc6cc2280e7fbd7
zechengh <zcheustc@gmail.com>
2020-07-10 00:45:46 -0400

    defense

--- defenses.sh
+++ defenses.sh
@@ -14,6 +14,15 @@ Dropout (){
     python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer ReLU2
 }
 
+Dropout_CIFAR (){
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer ReLU22 --iters 5000 --learning_rate 1e-2 --lambda_TV 1e1 --lambda_l2 0.0
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer ReLU22 --add_noise_to_input --iters 5000 --learning_rate 1e-2 --lambda_TV 1e1 --lambda_l2 0.0
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer ReLU12 --iters 5000 --learning_rate 1e-2 --lambda_TV 0.0 --lambda_l2 0.0
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer pool1 --iters 5000 --learning_rate 1e-2 --lambda_TV 0.0 --lambda_l2 0.0
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer conv22 --iters 5000 --learning_rate 1e-2 --lambda_TV 1e1 --lambda_l2 0.0
+    python inverse_whitebox_CIFAR_defense.py --noise_type dropout --layer pool2 --iters 5000 --learning_rate 1e-2 --lambda_TV 1e1 --lambda_l2 0.0
+}
+
 # Opt noise generation
 Opt_Noise_Generation (){
     python noise_generation_opt.py --noise_sourceLayer pool1 --noise_targetLayer conv2
@@ -33,8 +42,10 @@ Opt_Noise_Generation (){
 #Dropout
 #Opt_Noise_Generation
 
-python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer pool1
-python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer conv2
-python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer ReLU2
-python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer pool2
-python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer fc1
+#python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer pool1
+#python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer conv2
+#python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer ReLU2
+#python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer pool2
+#python inverse_whitebox_MNIST_defense.py --noise_type dropout --layer fc1
+
+Dropout_CIFAR
